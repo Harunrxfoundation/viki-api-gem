e6fa80a6e3e1cad2b6c281e41adf498ea4ca9383
Aysha Anggraini <ren.aysha@gmail.com>
2016-02-12 12:06:38 +0800

    Revert "Unsubscribe: add method to delete subscription"
    This reverts commit dfc228affe318bd266fa49abc739470504c9cb73.

--- lib/viki/viki_subscription.rb
+++ lib/viki/viki_subscription.rb
@@ -2,20 +2,14 @@ class Viki::VikiSubscription < Viki::Core::Base
   APPLIED_COUPONS = 'applied_coupons'
   GIFT_CARD = 'gift_card'
   PERIODS = 'periods'
-  UNSUBSCRIBE = 'delete_subscription'
 
   path '/viki_subscriptions', api_version: 'v5'
-  path '/viki_subscriptions/:viki_subscription_id', api_version: 'v5', name: UNSUBSCRIBE
   path '/viki_subscriptions/:viki_subscription_id/applied_coupons', api_version: 'v5', name: APPLIED_COUPONS
   path '/viki_subscriptions/:viki_subscription_id/gift_card', api_version: 'v5', name: GIFT_CARD
   path '/viki_subscriptions/:viki_subscription_id/periods', api_version: 'v5', name: PERIODS
 
   def self.applied_coupons(options={})
     self.fetch_sync(options.merge(named_path: APPLIED_COUPONS))
-  end
-
-  def self.unsubscribe(options={})
-    self.destroy_sync(options.merge(named_path: UNSUBSCRIBE))
   end
 
   def self.gift_card(options={})
