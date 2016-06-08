b3c58c31370081c9999590dc2672bc6d4e1183ae
Aysha Anggraini <ren.aysha@gmail.com>
2016-06-08 13:06:20 +0800

    Added an endpoint to verify that the coupon exist

--- lib/viki/viki_coupon.rb
+++ lib/viki/viki_coupon.rb
@@ -3,6 +3,7 @@ class Viki::VikiCoupon < Viki::Core::Base
 
   path '/viki_coupons', api_version: 'v5'
   path '/viki_coupons/:viki_coupon_id/applied_coupons', api_version: 'v5', name: APPLIED_COUPONS
+  path '/viki_coupons/coupon_codes/:viki_coupon_code', api_version: 'v5'
 
   def self.applied_coupons(options={})
     self.fetch_sync(options.merge(named_path: APPLIED_COUPONS))
