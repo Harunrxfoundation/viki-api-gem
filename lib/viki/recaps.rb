fa8d74cbfb6339354f807e3eaf82b9864ab1225b
Swapnil Kumar <swapnil.kumar@viki.com>
2016-08-26 14:05:06 +0800

    Adding Spec for Recap Endpoint

--- lib/viki/recaps.rb
+++ lib/viki/recaps.rb
@@ -3,19 +3,19 @@ class Viki::Recaps < Viki::Core::Base
   EDIT = 'update_recap'
   DELETE = 'delete_recap'
 
-  path '/recaps', api_version: "v4"
-  path '/recaps/:recap_id', api_version: "v4", name: EDIT
-  path '/recaps/:recap_id', api_version: "v4", name: DELETE
+  path '/recaps', api_version: 'v4'
+  path '/recaps/:recap_id', api_version: 'v4', name: EDIT
+  path '/recaps/:recap_id', api_version: 'v4', name: DELETE
 
   def self.create_recap(body = {}, &block)
     self.create({}, body, &block)
   end
 
   def self.update_recap(recap_id, body = {}, &block)
-    self.patch({recap_id: recap_id}.merge(named_path: EDIT), body, &block)
+    self.patch({ recap_id: recap_id }.merge(named_path: EDIT), body, &block)
   end
 
   def self.delete_recap(recap_id, &block)
-    self.destroy({recap_id: recap_id}.merge(named_path: DELETE), &block)
+    self.destroy({ recap_id: recap_id }.merge(named_path: DELETE), &block)
   end
 end
