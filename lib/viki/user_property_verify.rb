09805ec1ed07a20c46bd1dfc51165729ace5120b
Swapnil Kumar <swapnil.kumar@viki.com>
2016-10-06 15:26:38 +0800

    Reverting previous commit

--- lib/viki/user_property_verify.rb
+++ lib/viki/user_property_verify.rb
@@ -3,8 +3,8 @@ class Viki::UserPropertyVerify < Viki::Core::Base
   VERIFY = 'verify'
   path "/users/:user_id/verify", name: VERIFY
 
-  def self.resend_token(user_id, body = {}, &block)
-    self.create({user_id: user_id}.merge(named_path: VERIFY), body, &block)
+  def self.resend_token(user_id, verification_token, body = {}, &block)
+    self.create({user_id: user_id, verification_token: verification_token}.merge(named_path: VERIFY), body, &block)
   end
 
   def self.verify_token(user_id, body = {}, &block)
