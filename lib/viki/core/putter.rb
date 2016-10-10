b123b052648bb30e0524d2dacbd79ae8ebc15cc5
Duong Nguyen <ntd251@users.noreply.github.com>
2016-10-10 10:53:01 +0800

    Added Viki::Core::Putter

---
+++ lib/viki/core/putter.rb
@@ -0,0 +1,16 @@
+module Viki::Core
+  class Putter < BaseRequest
+    def on_complete(error, body, headers, &block)
+      block.call Viki::Core::Response.new(error, body)
+    end
+
+    def request
+      @request ||= Typhoeus::Request.new url,
+                                         body: body,
+                                         headers: default_headers,
+                                         method: "put",
+                                         # forbid_reuse: true,
+                                         timeout: (Viki.timeout_seconds_post)
+    end
+  end
+end
