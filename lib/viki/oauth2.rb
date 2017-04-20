0823f4d8c1746bf10b0ed293437a06f41c958682
Weiyuan Liu <weiyuan.liu@viki.com>
2017-04-20 14:50:06 +0800

    Remove validation, doing on main path method instead
    https://app.asana.com/0/275068761174408/312879493178880

--- lib/viki/oauth2.rb
+++ lib/viki/oauth2.rb
@@ -3,7 +3,6 @@ class Viki::OAuth2 < Viki::Core::Base
   path '/oauth/authorize', api_version: "v4", name: AUTHENTICATE_CLIENT
 
   def self.auth_client(body = {}, &block)
-    permitted_body = body.delete_if {|k,v| ![:client_id, :response_type, :redirect_uri, :scope, :state].include?(k) }
-    self.create({ named_path: AUTHENTICATE_CLIENT }, permitted_body, &block)
+    self.create({ named_path: AUTHENTICATE_CLIENT }, body, &block)
   end
 end
