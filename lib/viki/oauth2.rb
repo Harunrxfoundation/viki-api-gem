ff23957ba76c2fb2f79846d3e54b27ecda19e354
Weiyuan Liu <weiyuan.liu@viki.com>
2017-04-20 11:33:46 +0800

    Fixed changes to oath post call
    https://app.asana.com/0/275068761174408/312879493178880

--- lib/viki/oauth2.rb
+++ lib/viki/oauth2.rb
@@ -1,14 +1,9 @@
 class Viki::OAuth2 < Viki::Core::Base
   AUTHENTICATE_CLIENT = 'auth'
-  LOGIN = 'login'
   path '/oauth/authorize', api_version: "v4", name: AUTHENTICATE_CLIENT
-  path '/oauth/token', api_version: "v4", name: LOGIN
 
-  def self.auth_client(options = {}, &block)
-    self.fetch(options.merge(named_path: AUTHENTICATE_CLIENT), &block)
-  end
-
-  def self.login(options = {}, body = {}, &block)
-    self.create(options.merge(named_path: LOGIN), body, &block)
+  def self.auth_client(body = {}, &block)
+    permitted_body = body.permit(:client_id, :response_type, :redirect_uri, :scope, :state)
+    self.({ named_path: AUTHENTICATE_CLIENT }, permitted_body, &block)
   end
 end
