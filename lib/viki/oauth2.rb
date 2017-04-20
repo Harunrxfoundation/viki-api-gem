59c2c90f9f3dda2e50cea6e1c2bb223f42162a51
Weiyuan Liu <weiyuan.liu@viki.com>
2017-04-20 11:42:50 +0800

    Updated hash slice counterpart for testing on spec
    https://app.asana.com/0/275068761174408/312879493178880

--- lib/viki/oauth2.rb
+++ lib/viki/oauth2.rb
@@ -3,7 +3,7 @@ class Viki::OAuth2 < Viki::Core::Base
   path '/oauth/authorize', api_version: "v4", name: AUTHENTICATE_CLIENT
 
   def self.auth_client(body = {}, &block)
-    permitted_body = body.permit(:client_id, :response_type, :redirect_uri, :scope, :state)
-    self.({ named_path: AUTHENTICATE_CLIENT }, permitted_body, &block)
+    permitted_body = body.delete_if {|k,v| ![:client_id, :response_type, :redirect_uri, :scope, :state].include?(k) }
+    self.create({ named_path: AUTHENTICATE_CLIENT }, permitted_body, &block)
   end
 end
--- spec/viki/oauth2_spec.rb
+++ spec/viki/oauth2_spec.rb
@@ -1,19 +1,10 @@
 require 'spec_helper'
 
 describe Viki::OAuth2, api: true do
-  it "sends out a GET request for authorize" do
-    stub = stub_request('get', %r{.*/oauth/authorize.*})
+  it "sends out a POST request for authorizing client and retrieving credentials" do
+    stub = stub_request('post', %r{.*/oauth/authorize.*})
 
     described_class.auth_client({}) do
-    end
-    Viki.run
-    stub.should have_been_made
-  end
-
-  it "sends out a POST request for token retrieval" do
-    stub = stub_request('post', %r{.*/oauth/token.*})
-
-    described_class.login({}) do
     end
     Viki.run
     stub.should have_been_made
