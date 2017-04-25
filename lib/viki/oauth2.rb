6eef273a34a2503c3a2f7f9fefe4af1d86b872f2
Weiyuan Liu <weiyuan.liu@viki.com>
2017-04-25 11:06:45 +0800

    Fixed base routes as to updated requirement
    https://app.asana.com/0/275068761174408/312879493178880

--- lib/viki/oauth2.rb
+++ lib/viki/oauth2.rb
@@ -4,11 +4,15 @@ class Viki::OAuth2 < Viki::Core::Base
   path '/oauth/applications/uids/:uid', api_version: "v4", name: GET_CLIENT
   path '/oauth/authorize', api_version: "v4", name: AUTHENTICATE_CLIENT
 
-  def self.get_client(uid, &block)
+  def self.get_client(uid)
     self.fetch_sync({ named_path: GET_CLIENT, uid: uid })
   end
 
-  def self.auth_client(body = {}, &block)
+  def self.precheck_client(body = {})
+    self.fetch_sync(body.merge(named_path: AUTHENTICATE_CLIENT))
+  end
+
+  def self.auth_client(body = {})
     self.create_sync({ named_path: AUTHENTICATE_CLIENT }, body)
   end
 end
