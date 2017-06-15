bbfe5f72acd9c38607ea8cc77d1b10dff83c28d3
Liu, Weiyuan <weiyuan.liu@rakuten.com>
2017-06-15 17:27:34 +0800

    Updated key reference to plan_id on purchaseable_plans_invoice
    https://app.asana.com/0/inbox/275068761174407/357417901643269/365382563396225

--- lib/viki/purchasable_plan_invoice.rb
+++ lib/viki/purchasable_plan_invoice.rb
@@ -1,3 +1,3 @@
 class Viki::PurchasablePlanInvoice < Viki::Core::Base
-  path "/purchasable_plans/:purchasable_plan_id/invoice", api_version: "v5"
+  path "/purchasable_plans/:plan_id/invoice", api_version: "v5"
 end
--- spec/viki/purchaseable_plan_invoice_spec.rb
+++ spec/viki/purchaseable_plan_invoice_spec.rb
@@ -4,7 +4,7 @@ describe Viki::PurchasablePlanInvoice, api: true do
   it "gets an invoice for a purchasable plans of the current user" do
     stub = stub_request('get', %r{.*/purchasable_plans/21p/invoice.json.*})
 
-    described_class.fetch({ purchasable_plan_id: "21p" }) do
+    described_class.fetch({ plan_id: "21p" }) do
     end
     Viki.run
     stub.should have_been_made
