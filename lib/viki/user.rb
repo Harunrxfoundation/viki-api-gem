bfa0eb62daab82e7f0a58361658a84c1a78c8f97
Saarang <sahandfx@gmail.com>
2017-07-26 10:56:20 +0800

    Allow requesting login_history of users

--- lib/viki/user.rb
+++ lib/viki/user.rb
@@ -2,4 +2,9 @@ class Viki::User < Viki::Core::Base
   use_ssl
   cacheable
   path "/users"
+  path '/users/:user_id/login_history', api_version: "v4"
+
+  def self.login_history(options = {})
+    self.fetch_sync(options)
+  end
 end
